f85f87a8cd39302eb0b028ba5c2d6696ab1affaa
Philip Wright <wrightphilip4@gmail.com>
2023-12-21 13:33:06 +0100

    Show boostrap usage in test component

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -5,7 +5,8 @@ interface ContainerProps { }
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
     <div id="container">
-      <strong>Ready to create an app?</strong>
+      <strong>Used to test UI components</strong>
+      <button type="button" className="btn btn-primary">Primary</button>
       <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
     </div>
   );
