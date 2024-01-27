8667dd476209a683b0ffac64fd95edd831054f00
Philip Wright <wrightphilip4@gmail.com>
2024-01-27 17:09:53 +0100

    Destructuring

--- src/components/ElectionCard.tsx
+++ src/components/ElectionCard.tsx
@@ -2,24 +2,26 @@ import './ElectionCard.css';
 import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
 
 interface ElectionCard {
-    electionName: string,
-    electionDate: string,
-    electionSummary: string,
-    isRepeating: boolean,
-    repeatingEvery: string
+    electionProperties: {
+        electionName: string,
+        electionDate: string,
+        electionSummary: string,
+        isRepeating: boolean,
+        repeatingEvery: string
+    }
 }
 
-const ElectionCard: React.FC<ElectionCard> = (props) => {
+const ElectionCard: React.FC<ElectionCard> = ({ electionProperties }) => {
     return (
         <IonCard>
             <IonCardHeader>
-                <IonCardTitle>{props.electionProperties.electionName}</IonCardTitle>
+                <IonCardTitle>{electionProperties.electionName}</IonCardTitle>
             </IonCardHeader>
 
             <IonCardContent>
-                <b>Election Date: </b> {props.electionProperties.electionDate} <br></br>
-                <b>Election Summary: </b> {props.electionProperties.electionSummary} <br></br>
-                <b>Repeating every: </b> {props.electionProperties.repeatingEvery} <br></br>
+                <b>Election Date: </b> {electionProperties.electionDate} <br></br>
+                <b>Election Summary: </b> {electionProperties.electionSummary} <br></br>
+                <b>Repeating every: </b> {electionProperties.repeatingEvery} <br></br>
             </IonCardContent>
             <div className="row">
                 <div className="col">
