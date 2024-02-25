3d024c46f88c903e91845bb12ca222a06632afcf
Philip Wright <wrightphilip4@gmail.com>
2024-02-25 09:19:04 +0100

    Add basic template for countdown card logic

--- src/components/CountdownCard/CountdownCard.tsx
+++ src/components/CountdownCard/CountdownCard.tsx
@@ -1,12 +1,26 @@
 import '../CountdownCard/CountdownCard.css';
 import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
+import React, { useState, useEffect } from 'react';
 
 interface CountdownCard {
     countdownCardProperties: {
         electionDate: string,
     }
 }
+
 const CountdownCard: React.FC<CountdownCard> = ({ countdownCardProperties }) => {
+    const [valueToUpdate, setValueToUpdate] = useState(0);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setValueToUpdate(prevValue => prevValue + 1);
+        }, 1000);
+
+
+        return () => clearInterval(intervalId);
+    }, []); // The empty dependency array ensures that this effect runs only once on component mount
+
+
     return (
         <IonCard>
             <IonCardHeader>
@@ -14,7 +28,23 @@ const CountdownCard: React.FC<CountdownCard> = ({ countdownCardProperties }) => 
             </IonCardHeader>
 
             <IonCardContent>
-                CountdownCard Content
+                <div className="count-down-timer">
+                    <p>Countdown to election on</p>
+                    <div className="wrapper">
+                        <div className="description">
+                            <p>Days</p>
+                            <p>Hours</p>
+                            <p>Minutes</p>
+                            <p>Seconds</p>
+                        </div>
+                        <div className="times">
+                            <p>{valueToUpdate}</p>
+                            <p>2</p>
+                            <p>3</p>
+                            <p>4</p>
+                        </div>
+                    </div>
+                </div>
             </IonCardContent>
         </IonCard>
     );
