8178f5308cb43199ba0fb6d0daccc502b18b7e2f
Philip Wright <wrightphilip4@gmail.com>
2024-03-03 19:42:26 +0100

    Add checkbox and button

--- src/components/ReminderSetup/ReminderSetup.tsx
+++ src/components/ReminderSetup/ReminderSetup.tsx
@@ -1,7 +1,7 @@
 import RelativeDatePicker from '../RelativeDatePicker/RelativeDatePicker';
 import './ReminderSetup.css';
 import React, { useState } from 'react';
-import { IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonItem, IonLabel, IonSelect, IonSelectOption } from '@ionic/react';
+import { IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonItem, IonLabel, IonSelect, IonSelectOption, IonCheckbox, IonButtons, IonButton } from '@ionic/react';
 import AbsoluteDatePicker from '../AbsoluteDatePicker/AbsoluteDatePicker';
 interface ContainerProps { }
 
@@ -28,6 +28,14 @@ const ReminderSetup: React.FC<ContainerProps> = () => {
             <IonCardContent>
                 {dateTypeTerm === "Relative Date" && <RelativeDatePicker></RelativeDatePicker>}
                 {dateTypeTerm === "Absolute Date" && <AbsoluteDatePicker></AbsoluteDatePicker>}
+                <div className="row">
+                    <div className="text-start">
+                        <IonCheckbox>Use Phone Alarm</IonCheckbox>;
+                    </div>
+                    <div className="text-end">
+                        <IonButton >Create Reminder</IonButton>
+                    </div>
+                </div>
             </IonCardContent>
         </IonCard>
     );
