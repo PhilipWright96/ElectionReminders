ad361722b58b6a52703946a1666633d602ff0445
Philip Wright <wrightphilip4@gmail.com>
2024-03-29 20:13:29 +0100

    Add basic filter framework

--- src/pages/MyReminders.tsx
+++ src/pages/MyReminders.tsx
@@ -1,7 +1,15 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import { IonContent, IonHeader, IonItem, IonLabel, IonPage, IonTitle, IonToolbar, IonSelect, IonSelectOption, IonSearchbar, IonList } from '@ionic/react';
+import React, { useState } from 'react';
+import { FilterFields } from '../components/CountryElections/types';
 
 const MyReminders: React.FC = () => {
+    const filterFields: FilterFields = {
+        NAME: "name",
+        DATE: "date"
+    },
+        [filterTerm, setFilterTerm] = useState(""),
+        [filterTypeTerm, setFilterTypeTerm] = useState("name");
+
 
     return (
         <IonPage>
@@ -10,10 +18,25 @@ const MyReminders: React.FC = () => {
                     <IonTitle>My Reminders</IonTitle>
                 </IonToolbar>
             </IonHeader>
+            <IonItem>
+                <IonLabel>Filter By</IonLabel>
+                <IonSelect
+                    value={filterTypeTerm}
+                    onIonChange={(e) => setFilterTypeTerm(e.detail.value!)}
+                >
+                    <IonSelectOption value={filterFields.NAME}>Name</IonSelectOption>
+                    <IonSelectOption value={filterFields.DATE}>Date</IonSelectOption>
+                </IonSelect>
+            </IonItem>
+            <IonSearchbar
+                value={filterTerm}
+                placeholder='Filter reminders'>
+            </IonSearchbar>
             <IonContent className="ion-padding">
-                UI goes here...
+                <IonList>
+                </IonList>
             </IonContent>
-        </IonPage>
+        </IonPage >
     );
 };
 
