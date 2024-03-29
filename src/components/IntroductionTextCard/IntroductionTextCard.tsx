e526df8358017ba93c1d07bf01cb1b3d69d96cd0
Philip Wright <wrightphilip4@gmail.com>
2024-03-29 12:35:33 +0100

    Improve content of introduction text card

--- src/components/IntroductionTextCard/IntroductionTextCard.tsx
+++ src/components/IntroductionTextCard/IntroductionTextCard.tsx
@@ -6,16 +6,11 @@ interface ContainerProps { }
 const IntroductionTextCard: React.FC<ContainerProps> = () => {
     return (
         <IonCard>
-            <IonCardHeader>
-                <IonCardTitle>The App</IonCardTitle>
-            </IonCardHeader>
-
             <IonCardContent>
                 Welcome to Election Reminders - the app to help you remember
                 how and when to use your democratic rights. With this app, you can set up
                 countdowns and reminders for upcoming elections in various countries.
-                To add your first reminder, search a country in the right hand search
-                bar.
+                To add your first reminder, search a country in the search bar below.
             </IonCardContent>
         </IonCard>
     );
