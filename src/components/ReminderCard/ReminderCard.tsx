d9103c08058f55e69f0f78e2d3e2127e373b565b
Philip Wright <wrightphilip4@gmail.com>
2024-04-06 11:33:51 +0200

    Make reminder card reminder specific

--- src/components/ReminderCard/ReminderCard.tsx
+++ src/components/ReminderCard/ReminderCard.tsx
@@ -3,20 +3,26 @@ import './ReminderCard.css';
 import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle } from '@ionic/react';
 
 interface ReminderCard {
+    reminderProperties: {
+        reminderName: string,
+        reminderDate: string,
+        createdOn: string,
+        reminderDetails: string
+    }
 }
 
-const ReminderCard: React.FC<ReminderCard> = ({ }) => {
+const ReminderCard: React.FC<ReminderCard> = ({ reminderProperties }) => {
     return (
         <IonCard>
             <IonCardHeader>
-                <IonCardTitle>Reminder for Election a</IonCardTitle>
+                <IonCardTitle>{reminderProperties.reminderName}</IonCardTitle>
             </IonCardHeader>
 
             <IonCardContent>
                 <CountdownCard countdownCardProperties={{ countdownText: "Countdown to reminder which triggers on", countdownDate: new Date(2024, 2, 1) }}></CountdownCard>
-                <b>Election Date: </b> <br></br>
-                <b>Election Summary: </b> <br></br>
-                <b>Repeating every: </b> <br></br>
+                <b>Reminder Date: </b> {reminderProperties.reminderDate} <br></br>
+                <b>Created On: </b> {reminderProperties.createdOn} <br></br>
+                <b>Reminder Details: </b>{reminderProperties.reminderDetails} <br></br>
             </IonCardContent>
             <div className="row">
                 <div className="col">
