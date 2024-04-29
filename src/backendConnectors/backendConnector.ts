93126234204e9b2576eac2e73638e83028855954
Philip Wright <wrightphilip4@gmail.com>
2024-04-29 19:18:31 +0200

    Add backend communication methods to specfic file

---
+++ src/backendConnectors/backendConnector.ts
@@ -0,0 +1,22 @@
+export async function getDataFromBackend() {
+    const rest = await fetch("http://localhost:8080/test", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        }
+    }).then((res) => res.json())
+    console.log(rest);
+}
+
+export async function postDataToBackend() {
+    const rest = await fetch("http://localhost:8080/testPOST", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            testProperty: "testValue"
+        })
+    });
+    console.log(rest);
+}
