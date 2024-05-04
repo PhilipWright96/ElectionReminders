e9c92858cc3d8941a89a833f48f5d520b9b26439
Philip Wright <wrightphilip4@gmail.com>
2024-05-04 22:10:11 +0200

    Specify react version for eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,14 @@ import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    settings: {
+      react: {
+        version: "detect"
+      }
+    }
+  },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReactConfig,
