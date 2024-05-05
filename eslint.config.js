2a382d59acd83b9128386c98eabe5e2a8e09911d
PhilipWright96 <PhilipWright96@users.noreply.github.com>
2024-05-05 16:43:43 +0000

    Prettified Code!

--- eslint.config.js
+++ eslint.config.js
@@ -3,14 +3,13 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
-
 export default [
   {
     settings: {
       react: {
-        version: "detect"
-      }
-    }
+        version: "detect",
+      },
+    },
   },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
@@ -21,7 +20,7 @@ export default [
       // Rule disabled because we have typescript which essentially does the same thing
       "react/prop-types": "off",
       // Rule disabled because the React variable must be in scope already - otherwise the app wouldn't start
-      "react/react-in-jsx-scope": "off"
-    }
-  }
+      "react/react-in-jsx-scope": "off",
+    },
+  },
 ];
