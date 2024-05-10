54ec1591d367e231e2ff01d154488536d8e64e35
PhilipWright96 <PhilipWright96@users.noreply.github.com>
2024-05-10 20:21:09 +0000

    Prettified Code!

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -1,5 +1,5 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-// Disabling the "no undef" rule for eslint here since our 
+// Disabling the "no undef" rule for eslint here since our
 // tests don't work yet anyway
 /* eslint-disable-next-line no-alert, no-undef, semi */
 module.exports = {
