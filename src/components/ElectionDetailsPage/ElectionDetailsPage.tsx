04631d73ceb8c96cc6c93de6e310bdf1783e7a84
Philip Wright <wrightphilip4@gmail.com>
2024-05-11 13:57:01 +0200

    Add basic details version

--- src/components/ElectionDetailsPage/ElectionDetailsPage.tsx
+++ src/components/ElectionDetailsPage/ElectionDetailsPage.tsx
@@ -1,8 +1,6 @@
-import { IonHeader, IonPage, IonTitle, IonToolbar, IonContent, IonBackButton, IonButtons } from '@ionic/react';
+import { IonHeader, IonPage, IonTitle, IonToolbar, IonContent, IonBackButton, IonButtons, IonText, IonCard } from '@ionic/react';
 import React from 'react';
 import { RouteComponentProps } from 'react-router';
-import CountdownCard from '../CountdownCard/CountdownCard';
-import ReminderSetup from '../ReminderSetup/ReminderSetup';
 
 interface ElectionDetailsPageProperties extends RouteComponentProps<{ electionName: string }> { }
 
@@ -21,6 +19,22 @@ const ElectionDetailsPage: React.FC<ElectionDetailsPageProperties> = ({ match })
                 </IonToolbar>
             </IonHeader>
             <IonContent>
+                <div className="m-5">
+                    <IonCard>
+                        <IonText>
+                            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                                nisi ut aliquip ex ea commodo consequat.
+                                <br />
+                                <br />
+                                Duis aute irure dolor in
+                                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                                deserunt mollit anim id est laborum." </p>
+                        </IonText>
+                    </IonCard>
+                </div>
             </IonContent>
         </IonPage >
     );
