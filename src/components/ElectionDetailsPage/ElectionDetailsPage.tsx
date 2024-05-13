f739aa016b11803936f17f55e4c7628fbfcc8ce8
Philip Wright <wrightphilip4@gmail.com>
2024-05-13 19:50:53 +0200

    Remove unnecessary quotation marks

--- src/components/ElectionDetailsPage/ElectionDetailsPage.tsx
+++ src/components/ElectionDetailsPage/ElectionDetailsPage.tsx
@@ -22,7 +22,7 @@ const ElectionDetailsPage: React.FC<ElectionDetailsPageProperties> = ({ match })
                 <div className="m-5">
                     <IonCard>
                         <IonText>
-                            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
                                 nisi ut aliquip ex ea commodo consequat.
@@ -31,7 +31,7 @@ const ElectionDetailsPage: React.FC<ElectionDetailsPageProperties> = ({ match })
                                 Duis aute irure dolor in
                                 reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
                                 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                                deserunt mollit anim id est laborum." </p>
+                                deserunt mollit anim id est laborum. </p>
                         </IonText>
                     </IonCard>
                 </div>
