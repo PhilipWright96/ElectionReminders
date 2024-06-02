17aa8f279baa3603953701531b7551a9c9f9fb8c
Philip Wright <wrightphilip4@gmail.com>
2024-06-02 08:59:32 +0200

    Get election data from backend

--- src/backendConnectors/backendConnector.ts
+++ src/backendConnectors/backendConnector.ts
@@ -1,3 +1,5 @@
+import { ElectionData } from "../components/CountryElections/types";
+
 export async function getDataFromBackend() {
     const rest = await fetch("http://localhost:8080/test", {
         method: "GET",
@@ -6,6 +8,20 @@ export async function getDataFromBackend() {
         }
     }).then((res) => res.json())
     console.log(rest);
+}
+
+export async function getElectionDataFromBackend(): Promise<ElectionData[]> {
+    const testCountryName = "Germany",
+        urlSearchParams = new URLSearchParams({ countryName: testCountryName }),
+        url = `http://localhost:8080/electionsForCountry?${urlSearchParams.toString()}`;
+
+    const electionResultsFromBackend = await fetch(url, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        }
+    }).then((res) => res.json())
+    return await electionResultsFromBackend;
 }
 
 export async function postDataToBackend() {
