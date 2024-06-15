7c3f89cab05a33d838c7eb5854f6be982bd3a466
Philip Wright <wrightphilip4@gmail.com>
2024-06-15 13:58:16 +0200

    Add method for pulling in backend results

--- src/backendConnectors/backendConnector.ts
+++ src/backendConnectors/backendConnector.ts
@@ -1,4 +1,5 @@
 import { ElectionData } from "../components/CountryElections/types";
+import { ReminderData } from "../components/ReminderCard/types";
 
 const backendUrl = "http://localhost:8080";
 
@@ -25,6 +26,21 @@ export async function getElectionDataFromBackend(): Promise<ElectionData[]> {
     }).then((res) => res.json())
     return await electionResultsFromBackend;
 }
+
+export async function getReminderDataFromBackend(): Promise<ReminderData[]> {
+    const testUserId = "123",
+        urlSearchParams = new URLSearchParams({ userId: testUserId }),
+        url = `${backendUrl}/remindersForUser?${urlSearchParams.toString()}`;
+
+    const reminderResultsFromBackend = await fetch(url, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        }
+    }).then((res) => res.json())
+    return await reminderResultsFromBackend;
+}
+
 
 export async function postDataToBackend() {
     const rest = await fetch(`${backendUrl}/testPOST`, {
