938d7c3b7fbfa96e7b3de81aac39b4abd4490b09
Philip Wright <wrightphilip4@gmail.com>
2025-01-01 18:50:10 +0100

    Break up database logic

--- src/databaseConnectors/databaseConnector.ts
+++ src/databaseConnectors/databaseConnector.ts
@@ -1,6 +1,6 @@
 import { CapacitorSQLite } from '@capacitor-community/sqlite';
 
-const databaseName = "testDatabase6.db",
+const databaseName = "testDatabase.db",
     databaseProperties = {
         database: databaseName,
         version: 1,
@@ -8,14 +8,14 @@ const databaseName = "testDatabase6.db",
         readonly: false,
     },
     createTableQuery = `
-          CREATE TABLE IF NOT EXISTS reminders6 (
+          CREATE TABLE IF NOT EXISTS reminders (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL,
             email TEXT NOT NULL
           );
         `,
     insertQuery = `
-            INSERT INTO reminders6 (name, email)
+            INSERT INTO reminders (name, email)
             VALUES (?, ?);
             `;
 
@@ -24,39 +24,52 @@ const databaseName = "testDatabase6.db",
 export async function createReminderInDatabase(): Promise<void> {
     console.log("Creating reminder!");
     try {
-        console.log(`Creating db connection with ${databaseProperties}`)
-        await CapacitorSQLite.createConnection(databaseProperties);
-
-        console.log(`Opening db connection`)
-        await CapacitorSQLite.open({
-            database: databaseName,
-            readonly: false,
-        });
-
-        console.log(`Executing query ${createTableQuery}`);
-
-        await CapacitorSQLite.execute({
-            database: databaseName,
-            statements: createTableQuery,
-        });
-
-        console.log(`Adding new entry to database connection`);
-        const result = await CapacitorSQLite.run({
-            database: databaseName,
-            statement: insertQuery,
-            values: ["phil6", "philemail6"],
-        });
-
-        if (result.changes && result.changes.changes != undefined && result.changes.changes > 0) {
-            console.log(`User added successfully! Row ID: ${result.changes.lastId}`);
-        } else {
-            console.error("Failed to insert user.");
-        }
-        console.log(`Closing connection`);
-        await CapacitorSQLite.closeConnection({ database: databaseName, readonly: false });
+        await openDatabase();
+        await createOrUpdateReminderTable();
+        await addReminder();
+        await closeDatabase();
     }
     catch (error) {
         console.log(`error in database operation ${error}`);
     }
+}
 
+async function openDatabase(): Promise<void> {
+    console.log(`Creating db connection with ${databaseProperties}`)
+    await CapacitorSQLite.createConnection(databaseProperties);
+
+    console.log(`Opening db connection`)
+    await CapacitorSQLite.open({
+        database: databaseName,
+        readonly: false,
+    });
+}
+
+async function createOrUpdateReminderTable(): Promise<void> {
+    console.log(`Executing query ${createTableQuery}`);
+
+    await CapacitorSQLite.execute({
+        database: databaseName,
+        statements: createTableQuery,
+    });
+}
+
+async function addReminder(): Promise<void> {
+    console.log(`Adding user`);
+    const result = await CapacitorSQLite.run({
+        database: databaseName,
+        statement: insertQuery,
+        values: ["phil6", "philemail6"],
+    });
+
+    if (result.changes && result.changes.changes != undefined && result.changes.changes > 0) {
+        console.log(`User added successfully! Row ID: ${result.changes.lastId}`);
+    } else {
+        console.error("Failed to insert user.");
+    }
+}
+
+async function closeDatabase(): Promise<void> {
+    console.log(`Closing connection`);
+    await CapacitorSQLite.closeConnection({ database: databaseName, readonly: false });
 }
