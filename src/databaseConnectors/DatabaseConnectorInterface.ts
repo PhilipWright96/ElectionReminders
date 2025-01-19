850f4fa1f2165305bb05a09c802ab52b17272993
Philip Wright <wrightphilip4@gmail.com>
2025-01-19 14:17:51 +0100

    Add new interface method

--- src/databaseConnectors/DatabaseConnectorInterface.ts
+++ src/databaseConnectors/DatabaseConnectorInterface.ts
@@ -2,5 +2,6 @@ export interface DatabaseConnectorInterface {
     openDatabase(databaseName: string): Promise<void>,
     createOrUpdateReminderTable(databaseName: string): void,
     addReminder(databaseName: string, selectedReminderDateTime: Date, electionId: string): void,
+    readReminderTable(databaseName: string): Promise<any[]>,
     closeDatabase(databaseName: string): Promise<void>
 }
