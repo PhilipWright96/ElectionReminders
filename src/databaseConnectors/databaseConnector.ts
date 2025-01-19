7330b7a630616b31235b4451e5c579ee516c421a
Philip Wright <wrightphilip4@gmail.com>
2025-01-19 14:12:54 +0100

    Add first implementation of getting info from phone database

--- src/databaseConnectors/databaseConnector.ts
+++ src/databaseConnectors/databaseConnector.ts
@@ -16,3 +16,35 @@ export async function createReminderInDatabase(selectedReminderDateTime: Date, e
         console.log(`error in database operation ${error}`);
     }
 }
+
+// TODO - any is evil. Add a type here! 
+export async function getRemindersFromPhoneDatabase(): Promise<any> {
+    console.log("Retrieving reminders from local database!");
+    if (!enablePhoneTesting) {
+        console.log("Phone testing switched off - returning front end dummy data")
+        return dummyReminderData;
+    }
+    try {
+        const databaseConnector: DatabaseConnectorInterface = new SQLiteDatabaseConnector();
+        await databaseConnector.openDatabase(databaseName);
+        const remindersFromDatabase = await databaseConnector.readReminderTable(databaseName);
+        await databaseConnector.closeDatabase(databaseName);
+
+        console.log(`Returning reminders ${JSON.stringify(remindersFromDatabase)}`);
+        return mapDatabaseRemindersToFrontEndReminders(remindersFromDatabase);
+    }
+    catch (error) {
+        console.log(`error in database operation ${error}`);
+    }
+}
+
+// TODO - move this to the database connector implementation
+function mapDatabaseRemindersToFrontEndReminders(databaseReminders: any[]) {
+    return databaseReminders.map((databaseReminder) => ({
+        reminderName: databaseReminder.reminder_name,
+        electionName: databaseReminder.election_id,
+        reminderDetails: databaseReminder.reminder_details,
+        createdOn: databaseReminder.created_on,
+        reminderDate: databaseReminder.reminder_date,
+    }))
+}
