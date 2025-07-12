c81cc31a42285b78ad69ea99512aed73e4d8671b
Philip Wright <wrightphilip4@gmail.com>
2025-07-12 18:50:47 +0200

    Change styling for election details

--- src/components/ElectionDetailsPage/ElectionDetailsPage.tsx
+++ src/components/ElectionDetailsPage/ElectionDetailsPage.tsx
@@ -21,18 +21,20 @@ const ElectionDetailsPage: React.FC<ElectionDetailsPageProperties> = ({ match })
             <IonContent>
                 <div className="m-5">
                     <IonCard>
-                        <IonText>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                                nisi ut aliquip ex ea commodo consequat.
-                                <br />
-                                <br />
-                                Duis aute irure dolor in
-                                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                                deserunt mollit anim id est laborum. </p>
-                        </IonText>
+                        <div className="p-4">
+                            <IonText className="fs-4">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                                    nisi ut aliquip ex ea commodo consequat.
+                                    <br />
+                                    <br />
+                                    Duis aute irure dolor in
+                                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                                    deserunt mollit anim id est laborum. </p>
+                            </IonText>
+                        </div>
                     </IonCard>
                 </div>
             </IonContent>
