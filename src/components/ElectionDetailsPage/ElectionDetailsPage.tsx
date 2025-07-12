a29f4c5fa0c7f378e28eff3de0b1da2cb3711cad
Philip Wright <wrightphilip4@gmail.com>
2025-07-12 18:58:43 +0200

    Improve styling

--- src/components/ElectionDetailsPage/ElectionDetailsPage.tsx
+++ src/components/ElectionDetailsPage/ElectionDetailsPage.tsx
@@ -19,23 +19,19 @@ const ElectionDetailsPage: React.FC<ElectionDetailsPageProperties> = ({ match })
                 </IonToolbar>
             </IonHeader>
             <IonContent>
-                <div className="m-5">
-                    <IonCard>
-                        <div className="p-4">
-                            <IonText className="fs-4">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                                    nisi ut aliquip ex ea commodo consequat.
-                                    <br />
-                                    <br />
-                                    Duis aute irure dolor in
-                                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                                    deserunt mollit anim id est laborum. </p>
-                            </IonText>
-                        </div>
-                    </IonCard>
+                <div className="p-4">
+                    <IonText className="fs-4">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                            nisi ut aliquip ex ea commodo consequat.
+                            <br />
+                            <br />
+                            Duis aute irure dolor in
+                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                            deserunt mollit anim id est laborum. </p>
+                    </IonText>
                 </div>
             </IonContent>
         </IonPage >
