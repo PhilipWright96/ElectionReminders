397fcddae3323a9874bc7f226102bf390a2135f7
Philip Wright <wrightphilip4@gmail.com>
2026-05-08 13:42:35 +0200

    Improve dummy data

--- src/hooks/useDummyApi.tsx
+++ src/hooks/useDummyApi.tsx
@@ -7,7 +7,16 @@ export interface CountryInformation {
 }
 
 export const useDummyApi = () => {
-    const data = [{ Name: "EU" }, { Name: "Germany" }];
+    const data = [{
+        "electionName": "a",
+        "electionId": "a8e30f6d-788c-4c5a-b941-8d2e7eb248ed",
+        "electionDetails": "Election Details for A.",
+        "electionPollsOpenDateTime": new Date("2024-03-18T14:25:36"),
+        "electionPollsCloseDateTime": new Date("2025-03-18T14:25:36"),
+        "electionSummary": "Summary for A",
+        "countryName": "Germany",
+        "electionType": "COUNTRY"
+    }];
     return data;
 }
 
