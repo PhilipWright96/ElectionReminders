d78d4f568ea06318aaf9812b26cc050772043171
Philip Wright <wrightphilip4@gmail.com>
2026-05-08 13:42:18 +0200

    Improve filtering on search bar

--- src/components/CountrySearchBar/CountrySearchBar.tsx
+++ src/components/CountrySearchBar/CountrySearchBar.tsx
@@ -1,18 +1,21 @@
 import './CountrySearchBar.css';
-import { IonSearchbar, IonList, IonItem, IonLabel, IonIcon } from '@ionic/react';
+import { IonSearchbar, IonList, IonItem, IonLabel, IonIcon, IonSelect, IonSelectOption } from '@ionic/react';
 import React, { useState, useEffect } from 'react';
 import { useDummyApi, SearchResult } from '../../hooks/useDummyApi';
 import { checkboxOutline } from "ionicons/icons"
-import { countryMatchesSearchTerm } from './utils';
+import { countryMatchesSearchTerm, retrieveDataFromBackend } from './utils';
 import { useTranslation } from 'react-i18next';
+import { ElectionData } from '../CountryElections/types';
 interface ContainerProps { }
 
 const CountrySearchBar: React.FC<ContainerProps> = () => {
 
-    const initialData: SearchResult[] = [],
+    const initialData: ElectionData[] = [],
         debounceTimeInMilliseconds = 300,
         { t } = useTranslation(),
-        searchBarPlaceholder = t("Enter_country_name_here"),
+        //searchBarPlaceholder = t("Enter_country_name_here"),
+        [filterType, setFilterType] = useState("country"),
+        searchBarPlaceholder = filterType != "all" ? `Enter ${filterType} name here` : `Enter name here`,
         [searchTerm, setSearchTerm] = useState(""),
         [results, setResults] = useState(initialData);
 
@@ -21,16 +24,30 @@ const CountrySearchBar: React.FC<ContainerProps> = () => {
             setResults([]);
             return;
         }
-        const dummyCountryResults: SearchResult[] = useDummyApi(),
+        const results: ElectionData[] = useDummyApi(),
             dataMatchingUserSearchTerm =
-                dummyCountryResults.filter(({ Name }) =>
-                    countryMatchesSearchTerm(searchTerm, Name)
+                results.filter(({ countryName }) =>
+                    countryMatchesSearchTerm(searchTerm, countryName)
                 );
         setResults(dataMatchingUserSearchTerm);
     }, [searchTerm]);
 
     return (
         <>
+            <IonItem>
+                <IonLabel>Search Election Based On...</IonLabel>
+                <IonSelect
+                    value={filterType}
+                    onIonChange={(e) => setFilterType(e.detail.value!)}
+                >
+                    <IonSelectOption value="country">Country</IonSelectOption>
+                    <IonSelectOption value="region">Region</IonSelectOption>
+                    <IonSelectOption value="city">City</IonSelectOption>
+                    <IonSelectOption value="organization">Organization</IonSelectOption>
+                    <IonSelectOption value="all">All</IonSelectOption>
+                </IonSelect>
+            </IonItem >
+
             <div className="country-search-container">
                 <IonSearchbar
                     value={searchTerm}
@@ -44,8 +61,8 @@ const CountrySearchBar: React.FC<ContainerProps> = () => {
                 {results.length > 0 && (
                     <IonList>
                         {results.map((result) => (
-                            <IonItem key={result.Name} routerLink={`/countryElections/${result.Name}`}>
-                                <IonLabel>{result.Name}</IonLabel>
+                            <IonItem key={result.countryName} routerLink={`/countryElections/${result.countryName}`}>
+                                <IonLabel>{result.countryName}</IonLabel>
                                 <IonIcon slot="end" icon={checkboxOutline} />
                             </IonItem>
                         ))}
